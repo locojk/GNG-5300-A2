0cf1461f81df34bc5ae85f0c3243088750fb2dbc
locojk <gaojun5150@gmail.com>
2024-09-28 17:20:51 -0400

    finish admin

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from students.models import Student
 
-# Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'enrollment_date')
+
+admin.site.register(Student, StudentAdmin)
