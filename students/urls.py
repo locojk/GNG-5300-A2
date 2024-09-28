1aaa58457f32763a3b317a39d5f4261a2d3c20f9
locojk <gaojun5150@gmail.com>
2024-09-28 17:44:50 -0400

    Create views

---
+++ students/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.student_list, name='student_list'),  # List all students
+    path('<int:pk>/', views.student_detail, name='student_detail'),  # View a student's details
+    path('new/', views.student_create, name='student_create'),  # Add a new student
+    path('<int:pk>/edit/', views.student_edit, name='student_edit'),  # Edit a student's details
+]
+
