ef4aea14faa8bab84fff05f47717ccde268b3eb9
locojk <gaojun5150@gmail.com>
2024-09-28 17:03:53 -0400

    setting up the model

---
+++ students/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Students(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    date_of_birth = models.DateField()
+    enrollment_date = models.DateField()
+    grade = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
