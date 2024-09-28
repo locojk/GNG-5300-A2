08426ccabb10b4a9d43dce3c8d57d14c6dcceefd
locojk <gaojun5150@gmail.com>
2024-09-28 17:49:19 -0400

    add form

---
+++ students/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Student
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['first_name', 'last_name', 'email', 'date_of_birth', 'enrollment_date', 'grade']
+
+    # Custom validation for email
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email or '@' not in email:
+            raise forms.ValidationError("Please enter a valid email address.")
+        return email
+
+    # Custom validation for grade (between 1 and 12)
+    def clean_grade(self):
+        grade = self.cleaned_data.get('grade')
+        if grade < 1 or grade > 12:
+            raise forms.ValidationError("Grade must be between 1 and 12.")
+        return grade
--- students/views.py
+++ students/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Student
+from .forms import StudentForm
 
 # View to list all students
 def student_list(request):
@@ -14,41 +15,27 @@ def student_detail(request, pk):
 # View to add a new student
 def student_create(request):
     if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        email = request.POST['email']
-        date_of_birth = request.POST['date_of_birth']
-        enrollment_date = request.POST['enrollment_date']
-        grade = request.POST['grade']
-        
-        # Create and save new student
-        Student.objects.create(
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-            date_of_birth=date_of_birth,
-            enrollment_date=enrollment_date,
-            grade=grade
-        )
-        return redirect('student_list')
-
-    return render(request, 'students/student_form.html')
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm()
+    return render(request, 'students/student_form.html', {'form': form})
 
 # View to edit an existing student's information
 def student_edit(request, pk):
     student = get_object_or_404(Student, pk=pk)
     
     if request.method == 'POST':
-        student.first_name = request.POST['first_name']
-        student.last_name = request.POST['last_name']
-        student.email = request.POST['email']
-        student.date_of_birth = request.POST['date_of_birth']
-        student.enrollment_date = request.POST['enrollment_date']
-        student.grade = request.POST['grade']
-        student.save()
-        return redirect('student_detail', pk=student.pk)
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student_detail', pk=student.pk)
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'students/student_form.html', {'form': form})
 
-    return render(request, 'students/student_form.html', {'student': student})
 
 
 
